f1e69938f4398f862db78a0aeda725ddb015594a
Anas Rchid <archid-@student.1337.ma>
2020-08-21 12:23:05 +0100

    implement range and nearest

--- PointSET.java
+++ PointSET.java
@@ -2,7 +2,7 @@ import edu.princeton.cs.algs4.SET;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.RectHV;
 
 public class PointSET
@@ -34,45 +34,92 @@ public class PointSET
 
 	// draw all points to standard draw
 	public void draw() {
-		for (Point2D p : set) {
+		for (Point2D p : set)
 			p.draw();
-			StdOut.println(" >> " + p);
-		}
 	}
 
 	// all points that are inside the rectangle (or on the boundary)
 	public Iterable<Point2D> range(RectHV rect) {
 		if (rect == null)
 			throw new IllegalArgumentException("range: null argument");
-		return set;
+		SET<Point2D> in = new SET<Point2D>();
+		for (Point2D p : set) {
+			if (rect.contains(p))
+				in.add(p);
+		}
+		return in;
 	}
 
 	// a nearest neighbor in the set to point p; null if the set is empty
 	public Point2D nearest(Point2D p) {
-		if (p == null)
-			throw new IllegalArgumentException("nearest: null argument");
-		return p;
+		Point2D champ = null;
+		for (Point2D q : set) {
+			if (champ == null || q.distanceTo(p) < champ.distanceTo(p))
+				champ = q;
+		}
+		return champ;
 	}
 
 	// unit testing of the methods (optional)
 	public static void main(String[] args) {
-        RectHV rect = new RectHV(0.0, 0.0, 1.0, 1.0);
         StdDraw.enableDoubleBuffering();
-		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.setPenColor(StdDraw.BOOK_RED);
 		StdDraw.setPenRadius(0.01);
+
+		In in = new In(args[0]);
+        RectHV rect = null;
+		Point2D tmp = null;
+
         PointSET kdtree = new PointSET();
+		while (!in.isEmpty()) {
+			double x = in.readDouble();
+			double y = in.readDouble();
+			if (tmp == null) {
+				 tmp = new Point2D(x, y);
+				 continue;
+			} else if (rect == null) {
+				rect = new RectHV(Math.min(x, tmp.x()), Math.min(y, tmp.y()),
+								  Math.max(x, tmp.x()), Math.max(y, tmp.y()));
+				continue;
+			}
+			Point2D p = new Point2D(x, y);
+			kdtree.insert(p);
+			StdOut.println(p);
+		}
+		kdtree.draw();
+		StdDraw.setPenColor(StdDraw.BOOK_BLUE);
+		StdDraw.setPenRadius(0.001);
+		rect.draw();
+		StdDraw.show();
+
+		Point2D p = null;
         while (true) {
             if (StdDraw.isMousePressed()) {
                 double x = StdDraw.mouseX();
                 double y = StdDraw.mouseY();
-                StdOut.printf("%8.6f %8.6f\n", x, y);
-                Point2D p = new Point2D(x, y);
+				p = new Point2D(x, y);
 				StdOut.printf("%8.6f %8.6f\n", x, y);
-				kdtree.insert(p);
 				StdDraw.clear();
+				StdDraw.setPenColor(StdDraw.BOOK_RED);
+				StdDraw.setPenRadius(0.01);
 				kdtree.draw();
+				StdDraw.setPenColor(StdDraw.BOOK_BLUE);
+				StdDraw.setPenRadius(0.001);
+				rect.draw();
+				StdDraw.setPenColor(StdDraw.GREEN);
+				StdDraw.setPenRadius(0.013);
+				p.draw();
+				StdDraw.setPenColor(StdDraw.MAGENTA);
+				for (Point2D q : kdtree.range(rect))
+					q.draw();
+				Point2D near = kdtree.nearest(p);
+				if (near != null) {
+					StdDraw.setPenColor(StdDraw.YELLOW);
+					near.draw();
+				}
 				StdDraw.show();
-            }
+            } else if (p != null)
+				kdtree.insert(p);
             StdDraw.pause(20);
         }
 
