da545a82035304fcce4967c8bbebfecd4e65a1f4
Anas Rchid <archid-@student.1337.ma>
2020-09-13 16:20:26 +0100

    inmplement draw()

--- KdTree.java
+++ KdTree.java
@@ -29,13 +29,13 @@ public class KdTree
 
 		public boolean on_left(Point2D p, boolean is_vertical) {
 			return (is_vertical && point.x() > p.x())
-				|| (!isvertical && point.y() > p.y());
+				|| (!is_vertical && point.y() > p.y());
 		}
 
 	}
 
 	KdNode root;
-	long size_;
+	int size_;
 
 	// construct an empty set of points
 	public KdTree() { root = null; size_ = 0; }
@@ -46,13 +46,34 @@ public class KdTree
 	// number of points in the set
 	public int size() { return size_; }
 
+	private void draw(KdNode root) {
+		if (root != null) {
+			StdOut.println("drawing >> " + root.point);
+			if (root.is_vert) {
+				StdDraw.setPenColor(StdDraw.RED);
+				root.point.drawTo(new Point2D(root.point.x(), 1));
+				root.point.drawTo(new Point2D(root.point.x(), 0));
+			} else {
+				StdDraw.setPenColor(StdDraw.BLUE);
+				root.point.drawTo(new Point2D(1, root.point.y()));
+				root.point.drawTo(new Point2D(0, root.point.y()));
+			}
+			StdDraw.show();
+			StdDraw.pause(1000);
+			draw(root.left);
+			draw(root.right);
+		}
+	}
+
 	// draw all points to standard draw
 	public void draw() {
-
+		StdDraw.enableDoubleBuffering();
+		StdDraw.setPenRadius(0.001);
+		draw(root);
 	}
 
 	private KdNode make_tree(KdNode root, Point2D p, boolean is_vertical) {
-		if (!root)
+		if (root == null)
 			return new KdNode(null, p, null, is_vertical);
 		else if (root.equal_to(p))
 			return root;
@@ -87,7 +108,7 @@ public class KdTree
 
 	// all points that are inside the rectangle (or on the boundary)
 	public Iterable<Point2D> range(RectHV rect) {
-		return set;
+		return new SET<Point2D>();
 	}
 
 	// a nearest neighbor in the set to point p; null if the set is empty
@@ -97,66 +118,69 @@ public class KdTree
 
 	// unit testing of the methods (optional)
 	public static void main(String[] args) {
-        StdDraw.enableDoubleBuffering();
-		StdDraw.setPenColor(StdDraw.BOOK_RED);
-		StdDraw.setPenRadius(0.01);
+        // StdDraw.enableDoubleBuffering();
+		// StdDraw.setPenColor(StdDraw.BOOK_RED);
+		// StdDraw.setPenRadius(0.01);
 
 		In in = new In(args[0]);
-        RectHV rect = null;
-		Point2D tmp = null;
+        // RectHV rect = null;
+		// Point2D tmp = null;
 
         KdTree kdtree = new KdTree();
 		while (!in.isEmpty()) {
 			double x = in.readDouble();
 			double y = in.readDouble();
-			if (tmp == null) {
-				tmp = new Point2D(x, y);
-				continue;
-			} else if (rect == null) {
-				rect = new RectHV(Math.min(x, tmp.x()), Math.min(y, tmp.y()),
-								  Math.max(x, tmp.x()), Math.max(y, tmp.y()));
-				continue;
-			}
+			// if (tmp == null) {
+			// 	tmp = new Point2D(x, y);
+			// 	continue;
+			// } else if (rect == null) {
+			// 	rect = new RectHV(Math.min(x, tmp.x()), Math.min(y, tmp.y()),
+			// 					  Math.max(x, tmp.x()), Math.max(y, tmp.y()));
+			// 	continue;
+			// }
 			Point2D p = new Point2D(x, y);
 			kdtree.insert(p);
 			StdOut.println(p);
 		}
-		kdtree.draw();
-		StdDraw.setPenColor(StdDraw.BOOK_BLUE);
-		StdDraw.setPenRadius(0.001);
-		rect.draw();
-		StdDraw.show();
 
-		Point2D p = null;
-        while (true) {
-            if (StdDraw.isMousePressed()) {
-                double x = StdDraw.mouseX();
-                double y = StdDraw.mouseY();
-				p = new Point2D(x, y);
-				StdOut.printf("%8.6f %8.6f\n", x, y);
-				StdDraw.clear();
-				StdDraw.setPenColor(StdDraw.BOOK_RED);
-				StdDraw.setPenRadius(0.01);
-				kdtree.draw();
-				StdDraw.setPenColor(StdDraw.BOOK_BLUE);
-				StdDraw.setPenRadius(0.001);
-				rect.draw();
-				StdDraw.setPenColor(StdDraw.GREEN);
-				StdDraw.setPenRadius(0.013);
-				p.draw();
-				StdDraw.setPenColor(StdDraw.MAGENTA);
-				for (Point2D q : kdtree.range(rect))
-					q.draw();
-				Point2D near = kdtree.nearest(p);
-				if (near != null) {
-					StdDraw.setPenColor(StdDraw.YELLOW);
-					near.draw();
-				}
-				StdDraw.show();
-            } else if (p != null)
-				kdtree.insert(p);
-            StdDraw.pause(20);
-        }
+		kdtree.draw();
+		StdOut.println("#####");
+
+		// StdDraw.setPenColor(StdDraw.BOOK_BLUE);
+		// StdDraw.setPenRadius(0.001);
+		// rect.draw();
+		// StdDraw.show();
+
+		// Point2D p = null;
+        // while (true) {
+        //     if (StdDraw.isMousePressed()) {
+        //         double x = StdDraw.mouseX();
+        //         double y = StdDraw.mouseY();
+		// 		p = new Point2D(x, y);
+		// 		StdOut.printf("%8.6f %8.6f\n", x, y);
+		// 		StdDraw.clear();
+		// 		StdDraw.setPenColor(StdDraw.BOOK_RED);
+		// 		StdDraw.setPenRadius(0.01);
+		// 		kdtree.draw();
+		// 		StdDraw.setPenColor(StdDraw.BOOK_BLUE);
+		// 		StdDraw.setPenRadius(0.001);
+		// 		rect.draw();
+		// 		StdDraw.setPenColor(StdDraw.GREEN);
+		// 		StdDraw.setPenRadius(0.013);
+		// 		p.draw();
+		// 		StdDraw.setPenColor(StdDraw.MAGENTA);
+		// 		for (Point2D q : kdtree.range(rect))
+		// 			q.draw();
+		// 		Point2D near = kdtree.nearest(p);
+		// 		if (near != null) {
+		// 			StdDraw.setPenColor(StdDraw.YELLOW);
+		// 			near.draw();
+		// 		}
+		// 		StdDraw.show();
+        //     } else if (p != null)
+		// 		kdtree.insert(p);
+        //     StdDraw.pause(20);
+        // }
 
 	}
 
